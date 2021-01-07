bf19e4303fb829fa1e4f11c70215c72e63e386b6
mikkie383 <tsa16002@byui.edu>
2021-01-07 15:00:29 -0700

    corrected date

--- script.js
+++ script.js
@@ -4,4 +4,4 @@ const time = document.querySelector('.date');
 const date = new Date();
 
 year.textContent = date.getFullYear();
-time.innerHTML = `Last Updated: ${date.toLocaleDateString()} ${date.toLocaleTimeString()}`;
+time.innerHTML = `Last Updated: ${document.lastModified}`;
