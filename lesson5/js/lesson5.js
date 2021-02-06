f30e687bd83b7e1b7a043a3dd014221577165eb1
mikkie383 <tsa16002@byui.edu>
2021-02-05 17:26:08 -0700

    first draft

---
+++ lesson5/js/lesson5.js
@@ -0,0 +1,28 @@
+const year = document.querySelector('.year');
+const time = document.querySelector('.date');
+var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+
+const date = new Date();
+
+year.textContent = date.getFullYear();
+time.innerHTML = `Last Updated: ${date.toLocaleDateString("en-AU", options)}`;
+
+const hamButton = document.querySelector('.ham');
+const mainMenu = document.querySelector('.navigation');
+const pancake = document.getElementById('pancake');
+
+hamButton.addEventListener('click', () =>{
+    mainMenu.classList.toggle('responsive')
+}, false);
+
+window.onresize = () => {
+    if(window.innerWidth > 760){
+        
+        mainMenu.classList.remove('responsive');
+        
+    }
+};
+
+if(date.getDay() == 5){
+    pancake.style.display = "block";
+}
